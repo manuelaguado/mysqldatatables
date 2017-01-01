a307f9668010e85ded928ccdc919513afd1ea12a
Manuel Emiliano Aguado Meza <manuelaguado@msn.com>
2017-01-01 11:36:57 -0600

    Update MysqlDatatable.php

--- MysqlDatatable.php
+++ MysqlDatatable.php
@@ -12,23 +12,12 @@ class SSP {
 				if ( isset( $column['formatter'] ) ) {
 					$row[ $column['dt'] ] = $column['formatter']( $data[$i][ $name_column ], $data[$i] );
 				}else{
-					$row[ $column['dt'] ] = ( self::detectUTF8($data[$i][$name_column]) )? $data[$i][$name_column] : utf8_encode($data[$i][$name_column]);	
+					$row[ $column['dt'] ] = $data[$i][$name_column];	
 				}
 			}
 			$out[] = $row;
 		}
 		return $out;
-	}
-	static function detectUTF8($string){
-		return preg_match('%(?:
-			[\xC2-\xDF][\x80-\xBF]        # non-overlong 2-byte
-			|\xE0[\xA0-\xBF][\x80-\xBF]               # excluding overlongs
-			|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}      # straight 3-byte
-			|\xED[\x80-\x9F][\x80-\xBF]               # excluding surrogates
-			|\xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
-			|[\xF1-\xF3][\x80-\xBF]{3}                  # planes 4-15
-			|\xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
-			)+%xs', $string);
 	}
 	static function limit ( $request, $columns )
 	{
